774af52c43eb692964ede0fa86aadb9e66d6ae88
EvertonLDS815 <evertonlino1234@gmail.com>
2023-11-26 11:49:16 -0300

    feat: routes - Send Order - draft false

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import {Router} from 'express';
 import multer from 'multer';
 
-import {CreateUserController} from './controllers/user/CreateUserController'
+import {CreateUserController} from './controllers/user/CreateUserController';
 import {AuthUserController} from './controllers/user/AuthUserController';
 import {DetailUserController} from './controllers/user/DetailUserController'
 import {EditPassUserController} from './controllers/user/EditPassUserController';
@@ -13,6 +13,7 @@ import {CreateOrderController} from './controllers/order/CreateOrderController';
 import {RemoveOrderController} from './controllers/order/RemoveOrderController';
 import {AddItemController} from './controllers/order/AddItemController';
 import {RemoveItemController} from './controllers/order/RemoveItemController';
+import {SendOrderController} from './controllers/order/SendOrderController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -29,17 +30,18 @@ router.get('/me', isAuthenticated, new DetailUserController().handle);
 router.patch('/edit', isAuthenticated, new EditPassUserController().handle);
 
 // Rotas Categories
-router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 // Rotas Products
-router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
-router.get('/category/product', new ListByCategoryController().handle)
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
+router.get('/category/product', new ListByCategoryController().handle);
 
 // Rotas Orders
-router.post('/order', new CreateOrderController().handle)
-router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
-router.post('/order/add', new AddItemController().handle)
-router.delete('/order/remove', new RemoveItemController().handle)
+router.post('/order', new CreateOrderController().handle);
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+router.post('/order/add', new AddItemController().handle);
+router.delete('/order/remove', new RemoveItemController().handle);
+router.patch('/order/send', new SendOrderController().handle);
 
 export {router};
